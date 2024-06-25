daf72f17c634d8458776eadd0e094562ae96f306
gyojincompany <luckyred@gmail.com>
2024-06-25 11:44:35 +0900

    회원 가입 기능 추가

--- src/main/java/com/gyojincompany/mybatis/controller/MemberController.java
+++ src/main/java/com/gyojincompany/mybatis/controller/MemberController.java
@@ -1,14 +1,42 @@
 package com.gyojincompany.mybatis.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gyojincompany.mybatis.dao.MemberDao;
 
 @Controller
 public class MemberController {
 	
+	@Autowired //의존 자동 주입 DI
+	private SqlSession sqlSession;
+	
+	
 	@RequestMapping(value = "/join")
 	public String join() {
 		return "join";
+	}
+	
+	@RequestMapping(value = "/joinOk")
+	public String joinOk(HttpServletRequest request, Model model) {
+		
+		String mid = request.getParameter("mid");
+		String mpw = request.getParameter("mpw");
+		String mname = request.getParameter("mname");
+		String memail = request.getParameter("memail");
+		
+		MemberDao memberDao = sqlSession.getMapper(MemberDao.class);
+		memberDao.joinMemberDao(mid, mpw, mname, memail);
+		
+		model.addAttribute("mid", mid);
+		model.addAttribute("mname", mname);
+		
+		return "joinOk";
 	}
 
 }
---
+++ src/main/java/com/gyojincompany/mybatis/dao/MemberDao.java
@@ -0,0 +1,6 @@
+package com.gyojincompany.mybatis.dao;
+
+public interface MemberDao {
+	
+	public void joinMemberDao(String mid, String mpw, String mname, String memail);//회원 가입 메소드
+}
